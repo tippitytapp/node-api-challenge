2466117ac517633547e23ae3b39c3e44fb56592a
tippitytapp <marc.tapp@gmail.com>
2020-05-08 13:27:02 -0400

    mvp

--- index.js
+++ index.js
@@ -39,3 +39,4 @@ server.get('/', (req, res) => {
 server.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} mode on http://localhost:${PORT}`.magenta.underline.bold))
 
 server.use('/api/projects', ProjectsRouter);
+server.use('/api/projects', ActionsRouter);
